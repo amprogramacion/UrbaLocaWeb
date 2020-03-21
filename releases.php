1a53df9b965ad5862d47e80da5cf47bf24cd1eba
Angel <Angel@EQUIPO-ANGEL>
2020-03-21 22:07:34 +0100

    Actualizados links de descarga

--- releases.php
+++ releases.php
@@ -7,9 +7,9 @@
         <p>Esta versión no está en ningún repositorio, debido a que ya no recibe soporte de actualizaciones. No obstante, 
             <b>puedes descargar el Servidor, el Juego y la Web de nuestro foro</b>. En los próximos días publicaremos un tutorial básico de cómo conectar todos los elementos.</p>
         <ul>
-            <li><a href="http://urba-loca.com/foro/index.php?topic=4.0" target="_blank">Descargar UrbaServer 0.5 (Old-Release 2016) aquí</a></li>
-            <li>Descargar UrbaCMS (Old-Release 2016) aquí <b style="color: red;">(NO DISPONIBLE TODAVÍA)</b></li>
-            <li><a href="http://urba-loca.com/foro/index.php?topic=5.0" target="_blank">Descargar Game Engine (UrbaLoca AveFenix 2016) aquí</a></li>
+            <li><a href="https://github.com/amprogramacion/UrbaServer/releases" target="_blank">Descargar UrbaServer 0.5 (Old-Release 2016) aquí</a></li>
+            <li>Descargar UrbaCMS (Old-Release 2016) aquí</li>
+            <li><a href="https://github.com/amprogramacion/UrbaLoca/releases" target="_blank">Descargar Game Engine (UrbaLoca AveFenix 2016) aquí</a></li>
         </ul>
     </div>
 </div>
