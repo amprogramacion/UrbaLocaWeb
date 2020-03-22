15d6f8843634ef3690982121598e583c46bf1638
Angel <Angel@EQUIPO-ANGEL>
2020-03-22 19:38:55 +0100

    Cambiados enlaces

--- clases/MySQL.php
+++ clases/MySQL.php
@@ -6,7 +6,7 @@ class MySQL {
 
     public static function query($sql) {
         if (empty(self::$conexion)) {
-            $mysqli = mysqli_connect("localhost", "urbaloca", 'urbaloca', "urbaloca");
+            $mysqli = mysqli_connect("192.168.1.80", "root", 'r3set7ing', "urbaloca");
             if (mysqli_connect_errno($mysqli)) {
                 echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
             }
