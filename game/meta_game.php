88f10cd984624e27efec5ca6e0100174b834f38a
angel <angel@DESKTOP-GRRB6C1>
2020-05-31 19:17:37 +0200

    Cadena de conexion

--- game/meta_game.php
+++ game/meta_game.php
@@ -1,6 +1,7 @@
 <script src="/js/jquery.3.3.1.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 
+<script src="/js/websocket_in.js"></script>
 <script src="/js/websocket_listener.js"></script>
 <script src="/js/websocket_onopen.js"></script>
 <script src="/js/websocket_onmessage.js"></script>
