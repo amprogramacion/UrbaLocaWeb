9b9cfd0b1089ddb1578c6eeae412c2fe79694e28
angel <angel@DESKTOP-GRRB6C1>
2020-05-31 22:54:32 +0200

    Room

--- game/room.php
+++ game/room.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "<pre>".print_r($_REQUEST, true)."</pre>";
-
+$user = empty($_GET['user']) ? "escavo" : $_GET['user'];
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -9,9 +9,21 @@ echo "<pre>".print_r($_REQUEST, true)."</pre>";
         <?php
         include("meta_game.php");
         ?>
+        <style type="text/css">
+            .loko {
+                border: 2px solid black;
+                width: 50px;
+                height: 50px;
+                background-color: black;
+                color: white;
+                position: absolute;
+            }
+        </style>
     </head>
-    <body>
+    <body style="height: 98vh;">
         <div id="idl_websocket_status">Espera...</div>
-        <input type="text" id="urbaloca_username" value="escavo">
+        <input type="text" id="urbaloca_username" value="<?=$user;?>">
+        <input type="text" id="mix">
+        <input type="text" id="miy">
     </body>
 </html>
