7fd7de6403c49775a59170f2091fe27984fb35ae
angel <angel@DESKTOP-GRRB6C1>
2020-05-31 19:23:11 +0200

    Conexion al servidor

--- js/websocket_in.js
+++ js/websocket_in.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var username = $("#urbaloca_username").val();
 
   
-    miWebsocket = new WebSocket("ws://localhost:8080/" + username + "/");
+    miWebsocket = new WebSocket("ws://localhost:8080/UrbaServer2/urbaloca/" + username);
     
     miWebsocket.debug = true;
     miWebsocket.timeoutInterval = 5400;
