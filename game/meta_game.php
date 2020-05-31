ca51221a512a552662addc50135723fe6f920e10
angel <angel@DESKTOP-GRRB6C1>
2020-05-31 19:28:05 +0200

    get.js

--- game/meta_game.php
+++ game/meta_game.php
@@ -1,6 +1,8 @@
 <script src="/js/jquery.3.3.1.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 
+<script src="/js/get.js"></script>
+
 <script src="/js/websocket_in.js"></script>
 <script src="/js/websocket_listener.js"></script>
 <script src="/js/websocket_onopen.js"></script>
