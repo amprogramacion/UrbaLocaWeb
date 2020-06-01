7f4fbb4b0a8e2f72d9b18e0123e71eaa0501a461
angel <angel@DESKTOP-GRRB6C1>
2020-06-01 22:26:00 +0200

    Css para cada objeto

--- game/meta_game.php
+++ game/meta_game.php
@@ -1,4 +1,5 @@
 <script src="/js/jquery.3.3.1.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 
 <script src="/js/get.js"></script>
@@ -9,3 +10,9 @@
 <script src="/js/websocket_onmessage.js"></script>
 <script src="/js/websocket_onclose.js"></script>
 <script src="/js/rooms.js"></script>
+
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="/css/game/interface.css">
+<link rel="stylesheet" href="/css/game/char.css">
+<link rel="stylesheet" href="/css/game/navegador.css">
