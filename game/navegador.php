979ef1934baadbabd07c560b21f1782b369a1a7f
angel <angel@DESKTOP-GRRB6C1>
2020-06-01 22:26:46 +0200

    Css para cada objeto

--- game/navegador.php
+++ game/navegador.php
@@ -1,10 +1,17 @@
 <?php
 include("game_controller.php");
+$salas = Rooms::GetRooms();
 ?>
 <div class="navegador draggable">
     <div class="navegador-header drag-header">NAVEGADOR DE SALAS</div>
     <div class="navegador-body">
-        No hay salas
+        <?php
+        if(is_array($salas)) {
+            foreach($salas as $room) {
+                echo '<div data-id="'.$room['id'].'" class="room-itemlist">'.$room['nombre'].'</div>';
+            }
+        }
+        ?>
     </div>
 </div>
 <script>
