e2fed5d718a0fc59e2bc5f472808e1846d58c360
angel <angel@DESKTOP-GRRB6C1>
2020-06-01 22:26:31 +0200

    Css para cada objeto

--- game/game_controller.php
+++ game/game_controller.php
@@ -1,5 +1,4 @@
 <?php
-echo "<pre>".print_r($_SERVER['DOCUMENT_ROOT'], true)."</pre>";
 if(!function_exists("__autoload")) {
     function __autoload($clase) {
         $file = $_SERVER['DOCUMENT_ROOT']."/clases/".$clase.".php";
