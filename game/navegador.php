4f12bd3480df715a1288fcc6fee9737e86bd652a
angel <angel@DESKTOP-GRRB6C1>
2020-06-01 22:42:48 +0200

    Css para cada objeto

--- game/navegador.php
+++ game/navegador.php
@@ -2,20 +2,27 @@
 include("game_controller.php");
 $salas = Rooms::GetRooms();
 ?>
-<div class="window window-blue draggable">
-    <div class="window-header drag-header">NAVEGADOR DE SALAS</div>
+<div class="window window-blue draggable navegador">
+    <div class="window-header drag-header">
+        <span class="window-header-text">
+            NAVEGADOR DE SALAS
+        </span>
+        <span class="window-header-btn">
+            <a href="javascript:void(0);" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i></a>
+        </span>
+    </div>
     <div class="window-body">
         <?php
-        if(is_array($salas)) {
-            foreach($salas as $room) {
-                echo '<div data-id="'.$room['id'].'" class="room-itemlist">'.$room['nombre'].'</div>';
+        if (is_array($salas)) {
+            foreach ($salas as $room) {
+                echo '<div data-id="' . $room['id'] . '" class="room-itemlist">' . $room['nombre'] . '</div>';
             }
         }
         ?>
     </div>
 </div>
 <script>
-    $(document).ready(function() {
-       $( ".draggable" ).draggable({ containment: "body", scroll: false, handle: ".drag-header" });
+    $(document).ready(function () {
+        $(".draggable").draggable({containment: "body", scroll: false, handle: ".drag-header"});
     });
 </script>
