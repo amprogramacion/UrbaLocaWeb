323e810009ee9118423388e45507fdb0652fd9fc
angel <angel@DESKTOP-GRRB6C1>
2020-06-01 22:57:10 +0200

    Css para cada objeto

---
+++ game/footer.php
@@ -0,0 +1,12 @@
+<div id="footer">
+    <div class="row">
+        <div class="col-md-6">
+            <a href="javascript:void(0);" class="abrir_ventana boton_footer" data-id="navegador" title="Navegador"><i class="fa fa-h-square"></i></a>
+            <a href="javascript:void(0);" class="abrir_ventana boton_footer" data-id="trastero" title="Mis Muebles"><i class="fa fa-archive"></i></a>
+            <a href="javascript:void(0);" class="abrir_ventana boton_footer" data-id="catalogo" title="Catalogo"><i class="fa fa-shopping-cart"></i></a>
+        </div>
+        <div class="col-md-6">
+            <input type="text" id="chat_txt" class="form-control input-lg" placeholder="Escribe algo aquí!">
+        </div>
+    </div>
+</div>
