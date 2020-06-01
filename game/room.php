e73a2a2e1c3d0569ac4265436eba8a75f4abbf52
angel <angel@DESKTOP-GRRB6C1>
2020-06-01 16:10:03 +0200

    User Exit

--- game/room.php
+++ game/room.php
@@ -16,14 +16,44 @@ $user = empty($_GET['user']) ? "escavo" : $_GET['user'];
                 color: white;
                 position: absolute;
             }
+            #footer {
+                position: fixed;
+                bottom: 0;
+                width: 100%;
+                border-top: 2px solid black;
+                padding: 20px;
+            }
+            .navegador {
+                border: 2px solid black;
+                width: 350px;
+                padding: 10px;
+            }
+            .navegador-header {
+                border: 2px solid red;
+                margin-bottom: 10px;
+            }
+            .navegador-body {
+                border: 2px solid green;
+            }
         </style>
     </head>
-    <body style="height: 98vh;">
+    <body style="height: 98vh; padding: 0px; margin: 0px;">
         <div id="idl_websocket_status">Espera...</div>
-        <input type="hidden" id="urbaloca_username" value="<?=$user;?>">
+        <input type="hidden" id="urbaloca_username" value="<?= $user; ?>">
         <input type="hidden" id="mix">
         <input type="hidden" id="miy">
         <input type="hidden" id="charx">
         <input type="hidden" id="chary">
+        <?php
+        include("navegador.php");
+        ?>
+        <div id="footer">
+            <div class="row">
+                <div class="col-md-1">
+                    <a href="javascript:void(0);">SALAS</a>
+                </div>
+                
+            </div>
+        </div>
     </body>
 </html>
