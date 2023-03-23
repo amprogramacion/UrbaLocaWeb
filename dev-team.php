80dd819f0598243bf84e7edb21c0eb280252ae1a
Angel <amprogramacion@gmail.com>
2023-03-23 21:14:07 +0100

    IP neuroredacer

---
+++ dev-team.php
@@ -0,0 +1,18 @@
+<br><br><br><br>
+<div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+        <h1>¿Cómo entrar al equipo de desarrollo? ¿Qué necesitáis?</h1>
+        <p>Entrar al equipo de desarrollo es gratis y no hay que hacer ningún test de aptitud. <strong style="color: red;">No recibirás remuneración por tu ayuda. El proyecto es colaborativo de forma desinteresada.</strong></p>
+        <p>Necesitamos:</p>
+        <ul>
+            <li><strong>Programador PHP/JAVASCRIPT/NODEJS</strong>. Programador PHP/JAVASCRIPT. El CMS lo haremos en Symfony5 y la base de UrbaLoca estamos utilizando <a href="https://zimjs.com" target="_blank">ZIM JS</a>.</li>
+            <li><strong>Diseñador Gráfico</strong>. Podemos utilizar los png de hace 20 años, o bien nos ayudas tú con los diseños. Nosotros te lo agradeceremos poniendote en los créditos y no te copiaremos tu trabajo.</li>
+            <li><strong>Moderadores</strong>. Esto es obvio, pero no funcionará como hace 20 años. Las personas que REALMENTE COLABOREN con nosotros, ganarán rango. Ya sea ayudando en el foro, o en la comunidad. No habra "postulaciones" de momento. Simplemente mandame un email y lo vemos.</li>
+        </ul>
+        <h1>Materiales de diseño y backups... ¡¡TOOODOO!!</h1>
+        <p>La base de datos, el .fla original, los backups.... TODO lo que necesitas está aquí: <a href="https://mega.nz/folder/OZxw0ALB#nByZu7eb847fVWzY394QiQ" target="_blank">https://mega.nz/folder/OZxw0ALB#nByZu7eb847fVWzY394QiQ</a></p>
+        <h1>Grupo de Discord</h1>
+        <p>Entra en el grupo <strong>UrbaLoca Dev Team</strong> en Discord y participa en el desarrollo: <a href="https://discord.gg/5MGbpWJgeU" target="_blank">https://discord.gg/5MGbpWJgeU</a></p>
+    </div>
+</div>
