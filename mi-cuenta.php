e77f59926f4038be996998137520d8cf0d115f5a
Angel <amprogramacion@gmail.com>
2023-03-26 15:46:09 +0200

    Room v4

--- mi-cuenta.php
+++ mi-cuenta.php
@@ -8,8 +8,9 @@ include("controller.php");
 </div>
 
 <div class="row" style="text-align: center; padding: 40px;">
-    <h1>¡LA RELEASE 1 DE URBALOCA NO HA SALIDO TODAVÍA</h1>
-    <p>Pero te mantendremos informado de todas las actualizaciones...</p>
+    <h1>¡Ya ha salido la versión Beta de UrbaLoca 2023!</h1>
+    <p>Entra ahora!</p>
+    <a href="/game" class="btn btn-primary btn-lg">JUGAR AHORA</a>
     <br><br><br>
     
     <a href="/borrar-cuenta" class="btn btn-danger" onclick="javascript:seguro(this.href);return false;">Deseo borrar mi cuenta</a>
