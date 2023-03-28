cc48a37bc95c05b3c25d5b27dad594aed75de219
Angel <amprogramacion@gmail.com>
2023-03-28 02:04:16 +0200

    Room v4

---
+++ game/js/Casilla.js
@@ -0,0 +1,12 @@
+class Casilla {
+    constructor(x, y, w, h) {
+        this.src = "casilla.png?1";
+        this.img = null;
+        this.pos_x = 0;
+        this.pos_y = 0;
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+    }
+}
---
+++ game/js/Loko.js
@@ -0,0 +1,13 @@
+class Loko {
+    constructor(image_src) {
+        this.image_src = image_src;
+        this.img = null;
+        this.socket = null;
+        this.nombre = null;
+        this.id = null;
+        this.casillas = [];
+        this.caminos = [];
+        this.pos_x = null;
+        this.pos_y = null;
+    }
+}
